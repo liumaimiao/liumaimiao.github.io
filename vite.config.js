1d023272a4c8fe6a4ed767e5418d98aeb0a1e616
lyhistory <lyhistory@gmail.com>
2026-02-20 06:06:52 +0800

    fix blank page issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for safety
+  base: '/',
+  server: {
+    host: '127.0.0.1' // Forces IPv4 for localhost
+  }
 })
